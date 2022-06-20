b442360d89e17647c2cec886bfbba95fc3b72b2b
scndtnr <scndtnr@users.noreply.github.com>
2022-06-20 21:18:41 +0900

    Comment out top header.

--- frontend/src/components/page/Top/Top.page.tsx
+++ frontend/src/components/page/Top/Top.page.tsx
@@ -10,7 +10,7 @@ export const TopPage = () => {
         <meta name="description" content="オタクの1日についてのアンケート＆類型化アプリ" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Top />
+      {/* <Top /> */}
       <Form />
     </>
   );
